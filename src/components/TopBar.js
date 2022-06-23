7421d2d6b93a95f669c21f53004b9eff2c9933e3
Eric Lowe <ericlowe25@gmail.ocm>
2022-06-22 23:58:39 -0400

    added bootstrap and components

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,16 +1,23 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
 
-
-<Navbar bg="light" expand="lg" >
-    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
-        <img src="[Instacart logo src]" /> 
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto"> 
-        </Nav>
-        <Nav>
-            <Nav.Link href="#LogIn">Log In</Nav.Link>
-            <Button variant="success">Sign Up</Button>{' '}
-        </Nav>
+function TopBar() {
+ return(
+    <div>
+        <Navbar bg="light" expand="lg" style={{paddingTop: '0px', paddingBottom: '0px'}}>
+            <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home"><img src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+               <Nav className="mr-auto"> 
+               </Nav>
+              <Nav style={{padding: "24px 40px 20px 40px", fontSize: "18px", lineHeight: "26px", fontWeight: "bolder", display: "flex", marginLeft: "auto" }} >
+                <Nav.Link style={{color: "#343538", paddingRight: '15px'}} href="#LogIn">Log In</Nav.Link>
+                <Button variant="success" style={{fontSize: "18px", borderRadius: '20px'}}>Sign Up</Button>{' '}
+              </Nav>
     </Navbar.Collapse>
-</Navbar>
+ </Navbar>
+</div>
+)
+}
+export default TopBar
